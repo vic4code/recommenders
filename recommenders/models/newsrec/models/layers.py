70a23f11e946bf486aad530a348d5784f37f72d6
vic4code <vic4code@gmail.com>
2022-10-21 15:39:38 +0800

    Init fastformer modules

--- recommenders/models/newsrec/models/layers.py
+++ recommenders/models/newsrec/models/layers.py
@@ -1,9 +1,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+from tkinter import X
 import tensorflow.compat.v1.keras as keras
 from tensorflow.compat.v1.linalg import einsum
-from tensorflow.compat.v1.keras import layers
+from tensorflow.compat.v1.keras import layers, initializers
 from tensorflow.compat.v1.keras import backend as K
 
 
@@ -341,3 +342,296 @@ class OverwriteMasking(layers.Layer):
 
     def compute_output_shape(self, input_shape):
         return input_shape[0]
+
+
+class AttentionPooling(layers.Layer):
+
+    def __init__(self, hidden_size):
+        super(AttentionPooling, self).__init__()
+        self.att1 = layers.Dense(hidden_size)
+        self.att2 = layers.Dense(1)
+    
+    def call(self, inputs, attention_mask):
+        """
+        Args:
+            inputs: inputs (object): input tensor. (batch, seq_len, dim)
+        Return:
+            outputs: (batch, seq_len, dim)
+        """
+        batch_size = inputs.shape[0]
+        e = self.att1(inputs)
+        e = K.tanh(e)
+        alpha = self.att2(e)
+        alpha = K.exp(alpha)
+        if attention_mask is not None:
+            alpha = alpha * K.expand_dims(attention_mask, axis=2)
+        alpha = alpha / (K.sum(alpha, axis=1, keepdims=True) + 1e-8)
+        inputs = K.permute_dimensions(inputs, (0, 2, 1))
+        inputs = K.batch_dot(inputs, alpha, axes=[2, 1])
+        inputs = K.reshape(inputs, (batch_size, -1))
+
+        return inputs
+
+
+class FastSelfAttention(layers.Layer):
+
+    def __init__(self, nb_head, size_per_head, **kwargs):
+        self.nb_head = nb_head
+        self.size_per_head = size_per_head
+        self.output_dim = nb_head * size_per_head
+        self.now_input_shape=None
+        super(FastSelfAttention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.now_input_shape = input_shape
+        self.WQ = self.add_weight(name='WQ', 
+                                  shape=(input_shape[0][-1], self.output_dim),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+        self.WK = self.add_weight(name='WK', 
+                                  shape=(input_shape[1][-1], self.output_dim),
+                                  initializer='glorot_uniform',
+                                  trainable=True) 
+        self.Wq = self.add_weight(name='Wq', 
+                                  shape=(self.output_dim,self.nb_head),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+        self.Wk = self.add_weight(name='Wk', 
+                                  shape=(self.output_dim,self.nb_head),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+        
+        self.WP = self.add_weight(name='WP', 
+                                  shape=(self.output_dim,self.output_dim),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+        
+        
+        super(FastSelfAttention, self).build(input_shape)
+        
+    def call(self, inputs):
+        """
+        Args:
+            inputs: inputs (object): input tensor. (batch, seq_len)
+        Return:
+            outputs: (batch)
+        """
+
+        if len(inputs) == 2:
+            Q_seq, K_seq = inputs
+        elif len(inputs) == 4:
+            Q_seq, K_seq, Q_mask, K_mask = inputs #different mask lengths, reserved for cross attention
+
+        Q_seq = K.dot(Q_seq, self.WQ)        
+        Q_seq_reshape = K.reshape(Q_seq, (-1, self.now_input_shape[0][1], self.nb_head * self.size_per_head))
+
+        Q_att = K.permute_dimensions(K.dot(Q_seq_reshape, self.Wq), (0, 2, 1)) / self.size_per_head ** 0.5
+
+        if len(inputs)  == 4:
+            Q_att = Q_att - (1 - K.expand_dims(Q_mask, axis=1)) * 1e8
+
+        Q_att = K.softmax(Q_att)
+        Q_seq = K.reshape(Q_seq, (-1, self.now_input_shape[0][1], self.nb_head, self.size_per_head))
+        Q_seq = K.permute_dimensions(Q_seq, (0, 2, 1, 3))
+        
+        K_seq = K.dot(K_seq, self.WK)
+        K_seq = K.reshape(K_seq, (-1, self.now_input_shape[1][1], self.nb_head, self.size_per_head))
+        K_seq = K.permute_dimensions(K_seq, (0, 2, 1, 3))
+
+        Q_att = layers.Lambda(lambda x: K.repeat_elements(K.expand_dims(x, axis=3), self.size_per_head, axis=3))(Q_att)
+        global_q = K.sum(layers.multiply([Q_att, Q_seq]), axis=2)
+        
+        global_q_repeat = layers.Lambda(lambda x: K.repeat_elements(K.expand_dims(x, axis=2), self.now_input_shape[1][1], axis=2))(global_q)
+
+        QK_interaction = layers.multiply([K_seq, global_q_repeat])
+        QK_interaction_reshape = K.reshape(QK_interaction, (-1, self.now_input_shape[0][1], self.nb_head * self.size_per_head))
+        K_att = K.permute_dimensions(K.dot(QK_interaction_reshape, self.Wk), (0, 2, 1)) / self.size_per_head ** 0.5
+        
+        if len(inputs) == 4:
+            K_att = K_att - (1 - K.expand_dims(K_mask, axis=1)) * 1e8
+            
+        K_att = K.softmax(K_att)
+        K_att = layers.Lambda(lambda x: K.repeat_elements(K.expand_dims(x, axis=3), self.size_per_head, axis=3))(K_att)
+
+        global_k = K.sum(layers.multiply([K_att, QK_interaction]), axis=2)
+        global_k_repeat = layers.Lambda(lambda x: K.repeat_elements(K.expand_dims(x, axis=2), self.now_input_shape[0][1], axis=2))(global_k)
+        
+        #Q=V
+        QKQ_interaction = layers.multiply([global_k_repeat, Q_seq])
+        QKQ_interaction = K.permute_dimensions(QKQ_interaction, (0, 2, 1, 3))
+        QKQ_interaction = K.reshape(QKQ_interaction, (-1, self.now_input_shape[0][1], self.nb_head * self.size_per_head))
+        QKQ_interaction = K.dot(QKQ_interaction, self.WP)
+        QKQ_interaction = K.reshape(QKQ_interaction, (-1, self.now_input_shape[0][1], self.nb_head, self.size_per_head))
+        QKQ_interaction = K.permute_dimensions(QKQ_interaction, (0, 2, 1, 3))
+        QKQ_interaction = QKQ_interaction + Q_seq
+        QKQ_interaction = K.permute_dimensions(QKQ_interaction, (0, 2, 1, 3))
+        QKQ_interaction = K.reshape(QKQ_interaction, (-1, self.now_input_shape[0][1], self.nb_head * self.size_per_head))
+
+        return QKQ_interaction
+        
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0][0], input_shape[0][1], self.output_dim)
+
+
+class FastAttention(layers.Layer):
+
+    def __init__(self, nb_head, size_per_head, hidden_size, hidden_dropout_prob, layer_norm_eps):
+        super(FastAttention, self).__init__()
+        self.selfattn = FastSelfAttention(nb_head, size_per_head)
+        self.linear_module = keras.Sequential()
+        self.linear_module.add(layers.Dense(hidden_size))
+        self.linear_module.add(layers.Dropout(hidden_dropout_prob))
+        self.LayerNorm = layers.LayerNormalization(epsilon=layer_norm_eps)
+
+    def call(self, inputs, attention_mask):
+        """
+        Args:
+            inputs: inputs (object): input tensor. (batch, seq_len, dim)
+        Return:
+            outputs: (batch, seq_len, dim)
+        """
+        self_output = self.selfattn([inputs, inputs, attention_mask, attention_mask])
+        attention_output = self.linear_module(self_output)
+        attention_output = self.LayerNorm(attention_output + inputs)
+
+        return attention_output
+
+
+class FastformerLayer(layers.Layer):
+
+    def __init__(self, 
+                 nb_head, 
+                 size_per_head, 
+                 hidden_size, 
+                 intermediate_size, 
+                 hidden_dropout_prob, 
+                 layer_norm_eps):
+
+        super(FastformerLayer, self).__init__()
+        self.attention = FastAttention(nb_head, 
+                                       size_per_head, 
+                                       hidden_size,
+                                       hidden_dropout_prob, 
+                                       layer_norm_eps)
+
+        self.intermediate = layers.Dense(intermediate_size, activation='gelu')
+ 
+        self.linear_module = keras.Sequential()
+        self.linear_module.add(layers.Dense(hidden_size))
+        self.linear_module.add(layers.Dropout(hidden_dropout_prob))
+      
+        self.LayerNorm = layers.LayerNormalization(epsilon=layer_norm_eps)
+
+    def call(self, inputs, attention_mask):
+        attention_output = self.attention(inputs, attention_mask)
+        intermediate_output = self.intermediate(attention_output)
+        layer_output = self.linear_module(intermediate_output)
+        layer_output = self.LayerNorm(layer_output + attention_output)
+
+        return layer_output
+
+class FastformerEncoder(layers.Layer):
+    def __init__(self, 
+                 nb_head, 
+                 size_per_head, 
+                 hidden_size, 
+                 intermediate_size, 
+                 hidden_dropout_prob, 
+                 layer_norm_eps, 
+                 num_hidden_layers, 
+                 max_position_embeddings,
+                 pooler_type='weightpooler',
+                 pooler_count=1
+                 ):
+                       
+        super(FastformerEncoder, self).__init__()
+
+        self.encoders = []
+        for _ in range(num_hidden_layers):
+            self.encoders.append(
+                                FastformerLayer(
+                                    nb_head, 
+                                    size_per_head, 
+                                    hidden_size, 
+                                    intermediate_size,
+                                    hidden_dropout_prob, 
+                                    layer_norm_eps
+                                ) 
+            )
+
+        self.position_embeddings = layers.Embedding(max_position_embeddings, hidden_size)
+        self.LayerNorm = layers.LayerNormalization(epsilon=layer_norm_eps)
+        self.dropout = layers.Dropout(hidden_dropout_prob)
+
+        # support multiple different poolers with shared bert encoder.
+        self.poolers = []
+        if pooler_type == 'weightpooler':
+            for _ in range(pooler_count):
+                self.poolers.append(AttentionPooling(hidden_size))
+
+    def call(self, 
+             inputs, 
+             attention_mask, 
+             pooler_index=0
+             ):
+        #input_embs: batch_size, seq_len, emb_dim
+        #attention_mask: batch_size, seq_len, emb_dim
+
+        extended_attention_mask = K.expand_dims(attention_mask, axis=1)
+        # extended_attention_mask = attention_mask
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+
+        batch_size, seq_length, emb_dim = inputs.shape
+        position_ids = K.arange(seq_length, dtype='float32')
+        position_ids = K.expand_dims(position_ids, axis=0)
+        position_ids = K.repeat_elements(position_ids, batch_size, axis=0)
+        position_embeddings = self.position_embeddings(position_ids)
+
+        embeddings = inputs + position_embeddings
+        embeddings = self.LayerNorm(embeddings)
+        embeddings = self.dropout(embeddings)
+        all_hidden_states = [embeddings]
+
+        # print(all_hidden_states)
+        print(all_hidden_states[-1].shape, extended_attention_mask.shape)
+
+        for i, layer_module in enumerate(self.encoders):
+            layer_outputs = layer_module(all_hidden_states[-1], extended_attention_mask)
+            all_hidden_states.append(layer_outputs)
+        assert len(self.poolers) > pooler_index
+        output = self.poolers[pooler_index](all_hidden_states[-1], attention_mask)
+
+        return output 
+
+class Fastformer(layers.Layer):
+
+    def __init__(self,
+                 nb_head, 
+                 size_per_head, 
+                 hidden_size, 
+                 intermediate_size, 
+                 hidden_dropout_prob, 
+                 layer_norm_eps, 
+                 num_hidden_layers, 
+                 max_position_embeddings,
+                 pooler_type='weightpooler',
+                 pooler_count=1
+                 ):
+
+        super(Fastformer, self).__init__()
+   
+        self.word_embedding = layers.Embedding(max_position_embeddings, hidden_size, mask_zero=True)
+        self.fastformer_model = FastformerEncoder(nb_head, 
+                                                  size_per_head, 
+                                                  hidden_size, 
+                                                  intermediate_size, 
+                                                  hidden_dropout_prob, 
+                                                  layer_norm_eps, 
+                                                  num_hidden_layers, 
+                                                  max_position_embeddings,
+                                                  pooler_type='weightpooler',
+                                                  pooler_count=1)
+
+    def call(self, inputs, mask):
+        text_vec = self.fastformer_model(inputs, mask)
+        return text_vec
